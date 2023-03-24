32c7236eee17b975bc83f20adadf5100701be4fb
David de la Harpe <118104692+DDLH-92@users.noreply.github.com>
2023-03-24 18:14:37 +0100

    ToDoList

--- dymavue/src/App.vue
+++ dymavue/src/App.vue
@@ -1,21 +1,55 @@
 <template>
- <ul>
-  <template v-for="({prenom, id}, index) of utilisateurs" :key="id">
-    <li v-if="prenom != 'Paul'">{{ prenom }}</li>
-  </template>
- </ul>
+  <form @submit.prevent="ajoutTodo">
+    <label for="add-todo">Ajouter une tâche</label>
+    <input v-model="tache" id="add-todo" placeholder="Tâche à effectuer..." />
+    <button>Ajouter</button>
+  </form>
+  <ul>
+    <template v-for="(todo, index) in todos" :key="todo.id">
+      <li>
+        {{ todo.titre }}
+        <button @click="suppTodo(todo.id)">Supprimer</button>
+      </li>
+    </template>
+  </ul>
 </template>
 
-<script setup lang="ts">
-import { reactive } from "vue";
 
-const utilisateurs = reactive( [
-  {prenom: 'Jean', id: 1},
-  {prenom: 'Paul', id: 2},
-  {prenom: 'Pierre', id: 3},
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const tache = ref('');
+const todos = ref([
+  {
+    id: 1,
+    titre: 'Apprendre VueJS3',
+  },
+  {
+    id: 2,
+    titre: 'Réussir le test technique',
+  },
+  {
+    id: 3,
+    titre: 'Décrocher le stage :)',
+  },
+
 ]);
 
-setTimeout(() => {
-  utilisateurs.push({prenom: 'Julie', age: 40})
-}, 3000);
+let nouvelleTacheId = 4;
+
+function ajoutTodo() {
+  todos.value.push({
+    id: nouvelleTacheId++,
+    titre: tache.value,
+  });
+  tache.value = '';
+}
+
+function suppTodo(id) {
+  const index = todos.value.findIndex((todo) => todo.id === id);
+  if (index !== -1) {
+    todos.value.splice(index, 1);
+  }
+}
+
 </script>
