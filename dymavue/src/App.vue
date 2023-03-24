49b67d7983508abe40d27821a663ae9ca5a7efc1
David de la Harpe <118104692+DDLH-92@users.noreply.github.com>
2023-03-24 18:01:58 +0100

    list v-if

--- dymavue/src/App.vue
+++ dymavue/src/App.vue
@@ -1,29 +1,21 @@
 <template>
-  <ul>
-    <li v-for="(fruit, index) in fruits">{{ `${index} - ${fruit}` }}</li>
-  </ul>
-  <ul>
-    <li v-for="({prenom, notes}, index) in utilisateurs">{{ `${index} : ${prenom}` }}</li>
-  </ul>
-  <ul>
-    <li v-for="(valeur, cle, index) of voiture">{{ `${index} : ${valeur} - ${cle}` }}</li>
-  </ul>
-  <ul>
-    <li v-for="n in 4">Bonjour !</li>
-  </ul>
+ <ul>
+  <template v-for="({prenom, id}, index) of utilisateurs" :key="id">
+    <li v-if="prenom != 'Paul'">{{ prenom }}</li>
+  </template>
+ </ul>
 </template>
 
 <script setup lang="ts">
 import { reactive } from "vue";
 
-const fruits = reactive(['fraise','pomme', 'kiwi']);
-const utilisateurs = reactive([
-  { prenom: 'Jean', notes: [14, 15, 16, 18]},
-  { prenom: 'Paul', notes: [12, 10, 6, 8]},
-  { prenom: 'Pierre', notes: [3, 15, 18, 2]}
+const utilisateurs = reactive( [
+  {prenom: 'Jean', id: 1},
+  {prenom: 'Paul', id: 2},
+  {prenom: 'Pierre', id: 3},
 ]);
-const voiture = {
-  roues: 4,
-  prix: 4000,
-};
+
+setTimeout(() => {
+  utilisateurs.push({prenom: 'Julie', age: 40})
+}, 3000);
 </script>
