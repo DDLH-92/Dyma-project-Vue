229de78b7b4b812829924dc858dfa80f3f6d824c
David de la Harpe <118104692+DDLH-92@users.noreply.github.com>
2023-03-24 15:13:25 +0100

    watch function

--- dymavue/src/App.vue
+++ dymavue/src/App.vue
@@ -1,21 +1,32 @@
 <template>
   <input type="text" v-model="product.quantity">
-  <h1>{{ text }}</h1>
 <h2>Prix total HT : {{ totalPriceHT }}</h2>
 <h2>Prix total TTC : {{ totalPriceTTC }}</h2>
 </template>
 
 <script setup lang="ts">
-import { computed, reactive, ref } from "vue";
+import { computed, reactive, watch, ref } from "vue";
 
 const product = reactive({
   name: 'books',
   quantity: 3,
-  priceHT: 10
+  priceHT: 10,
+  nbrOfModification: 0
 })
+
+const price = ref(0);
 
 const totalPriceHT = computed(() => product.priceHT * product.quantity)
 const totalPriceTTC = computed (() => product.priceHT * product.quantity * 1.2)
+
+const unwatch = watch ([() => product.quantity, price], (newValue, oldValue) => {
+  product.nbrOfModification++;
+  console.log(newValue);
+  console.log(oldValue);
+  unwatch();
+})
+
+
 
 </script>
 
