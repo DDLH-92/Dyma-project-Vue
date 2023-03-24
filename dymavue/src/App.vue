6a3b9d8e2e8616fc069118c984aabbac505df18b
David de la Harpe <118104692+DDLH-92@users.noreply.github.com>
2023-03-24 12:59:30 +0100

    v-bind

--- dymavue/src/App.vue
+++ dymavue/src/App.vue
@@ -1,9 +1,20 @@
 <template>
-  <h1>Bonjour {{ name }}</h1>
+<h1 v-bind="obj">Bonjour monde !</h1>
+<input :type="inputType">
 </template>
 
 <script setup lang="ts">
-const name = 'David'
+
+const key = 'class';
+const h1title = 'montitre2';
+const h1id = 'unid2';
+
+const obj = {
+  [key ]: h1title,
+  'id': h1id
+}
+
+const inputType = 'number';
 </script>
 
 <style></style>
