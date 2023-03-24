97508bd5be61e489cdeecd7fc08b24e6f818ae3c
David de la Harpe <118104692+DDLH-92@users.noreply.github.com>
2023-03-24 18:27:08 +0100

    v-memo

--- dymavue/src/App.vue
+++ dymavue/src/App.vue
@@ -1,55 +1,11 @@
 <template>
-  <form @submit.prevent="ajoutTodo">
-    <label for="add-todo">Ajouter une tâche</label>
-    <input v-model="tache" id="add-todo" placeholder="Tâche à effectuer..." />
-    <button>Ajouter</button>
-  </form>
-  <ul>
-    <template v-for="(todo, index) in todos" :key="todo.id">
-      <li>
-        {{ todo.titre }}
-        <button @click="suppTodo(todo.id)">Supprimer</button>
-      </li>
-    </template>
-  </ul>
+  <h1 v-memo="[title]"><p>{{ title }}</p></h1>
+  <input type="text" v-model="input">
+  <p>{{ input }}</p>
 </template>
 
-
 <script setup lang="ts">
-import { ref } from 'vue';
-
-const tache = ref('');
-const todos = ref([
-  {
-    id: 1,
-    titre: 'Apprendre VueJS3',
-  },
-  {
-    id: 2,
-    titre: 'Réussir le test technique',
-  },
-  {
-    id: 3,
-    titre: 'Décrocher le stage :)',
-  },
-
-]);
-
-let nouvelleTacheId = 4;
-
-function ajoutTodo() {
-  todos.value.push({
-    id: nouvelleTacheId++,
-    titre: tache.value,
-  });
-  tache.value = '';
-}
-
-function suppTodo(id) {
-  const index = todos.value.findIndex((todo) => todo.id === id);
-  if (index !== -1) {
-    todos.value.splice(index, 1);
-  }
-}
-
+import { ref } from 'vue'
+const title = ref('Je suis un titre')
+const input = ref('')
 </script>
