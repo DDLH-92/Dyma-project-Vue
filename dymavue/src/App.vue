5c33b708c807b13dea0909cb2c5bbac44a967d6d
David de la Harpe <118104692+DDLH-92@users.noreply.github.com>
2023-03-24 14:25:20 +0100

    count reactiv

--- dymavue/src/App.vue
+++ dymavue/src/App.vue
@@ -1,30 +1,32 @@
 <template>
+  <h1>Bonjour {{ user.name }}</h1>
+  <h2>Count : {{ state.count }}</h2>
+  <button @click="incCount">add</button>
 </template>
 
 <script setup lang="ts">
+import { nextTick, reactive } from "vue";
 
-const obj = { name: 'Jean', age: 12 };
 
-function render() {
-  console.log('Should update template')
+const state = reactive ({
+  user: {
+  name : 'Tintin',
+  age : 15
+},
+count: 0
+})
+
+let {user, count } = state;
+
+console.log(count === state.count)
+
+function incCount() {
+  user.name = user.name === 'Tintin' ? 'Milou' : 'Tintin'
+  state.count++;
+  nextTick(() => {
+    console.log('tick')
+  })
 }
-const proxy = new Proxy(obj, {
-  get(obj: any, prop) {
-    render();
-    return obj[prop];
-    }
-  });
-
-set(obj: any, prop, value) {
-  render ();
-  obj[prop] = value;
-  return true;
-}
-
-proxy.age = 15
-
-console.log(proxy.name);
-console.log(proxy.age);
 
 </script>
 
