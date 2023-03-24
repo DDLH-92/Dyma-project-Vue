183f53ff2f18f2a8534622aeada0e36075070a79
David de la Harpe <118104692+DDLH-92@users.noreply.github.com>
2023-03-24 17:37:37 +0100

    v-for

--- dymavue/src/App.vue
+++ dymavue/src/App.vue
@@ -1,18 +1,29 @@
 <template>
-<button @click="toggle = !toggle">Toggle</button>
-<h1 v-show="toggle">Bonjour le monde !</h1>
-<h2 v-pre>{{ variable1 }}</h2>
+  <ul>
+    <li v-for="(fruit, index) in fruits">{{ `${index} - ${fruit}` }}</li>
+  </ul>
+  <ul>
+    <li v-for="({prenom, notes}, index) in utilisateurs">{{ `${index} : ${prenom}` }}</li>
+  </ul>
+  <ul>
+    <li v-for="(valeur, cle, index) of voiture">{{ `${index} : ${valeur} - ${cle}` }}</li>
+  </ul>
+  <ul>
+    <li v-for="n in 4">Bonjour !</li>
+  </ul>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { reactive } from "vue";
 
-const toggle = ref(true);
-const variable1 = ref('une valeur');
-
+const fruits = reactive(['fraise','pomme', 'kiwi']);
+const utilisateurs = reactive([
+  { prenom: 'Jean', notes: [14, 15, 16, 18]},
+  { prenom: 'Paul', notes: [12, 10, 6, 8]},
+  { prenom: 'Pierre', notes: [3, 15, 18, 2]}
+]);
+const voiture = {
+  roues: 4,
+  prix: 4000,
+};
 </script>
-
-<style scoped lang = "scss">
-
-
-</style>
