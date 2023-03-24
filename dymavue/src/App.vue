284ae349fbfa6ea74412415fad8b7ad5a4b548bd
David de la Harpe <118104692+DDLH-92@users.noreply.github.com>
2023-03-24 13:15:35 +0100

    base notion Typescript

--- dymavue/src/App.vue
+++ dymavue/src/App.vue
@@ -1,20 +1,27 @@
 <template>
-<h1 v-bind="obj">Bonjour monde !</h1>
-<input :type="inputType">
 </template>
 
 <script setup lang="ts">
 
-const key = 'class';
-const h1title = 'montitre2';
-const h1id = 'unid2';
-
-const obj = {
-  [key ]: h1title,
-  'id': h1id
+interface User {
+  name: string,
+  age: number | string
 }
 
-const inputType = 'number';
+let user: User | null;
+
+function createUser(name: string): User {
+  return {
+    name: 'David',
+    age: 18
+  }
+}
+
+user = createUser('David')
+
+user.age
+
 </script>
+
 
 <style></style>
