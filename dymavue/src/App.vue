81a12de52b5a4d79561c26f2c56ec07a7c9b88ce
David de la Harpe <118104692+DDLH-92@users.noreply.github.com>
2023-03-24 15:55:30 +0100

    style1

--- dymavue/src/App.vue
+++ dymavue/src/App.vue
@@ -1,51 +1,12 @@
 <template>
-  <input type="text" v-model="product.quantity">
-<h2>Prix total HT : {{ totalPriceHT }}</h2>
-<h2>Prix total TTC : {{ totalPriceTTC }}</h2>
+<h1 :class :style>Bonjour le monde !</h1>
 </template>
 
 <script setup lang="ts">
-import { computed, reactive, watch, ref, watchEffect } from "vue";
-
-
-interface Product {
-  name: string,
-  quantity: number,
-  priceHT: number,
-  nbrOfModification: number,
-  lastModification: number
-}
-
-const product = reactive<Product>({
-  name: 'books',
-  quantity: 3,
-  priceHT: 10,
-  nbrOfModification: 0,
-  lastModification: null
-})
-
-const price = ref(0);
-
-const totalPriceHT = computed(() => product.priceHT * product.quantity)
-const totalPriceTTC = computed (() => product.priceHT * product.quantity * 1.2)
-
-const unwatch = watch ([() => product.quantity, price], (newValue, oldValue) => {
-  product.nbrOfModification++;
-  console.log(newValue);
-  console.log(oldValue);
-})
-
-setTimeout(() => {
-
-  watchEffect(() => {
-    console.log('inwatchEffect')
-    product.priceHT = price.value
-    product.lastModification = Date.now()
-    console.log(product);
-  })
-}, 3000);
 
 </script>
 
 
-<style></style>
+<style scoped>
+@import "./assets/base.css"
+</style>
