97df0951a303f4741eab402be5dda62defb79ef8
David de la Harpe <118104692+DDLH-92@users.noreply.github.com>
2023-03-24 16:26:31 +0100

    classes css

--- dymavue/src/App.vue
+++ dymavue/src/App.vue
@@ -1,12 +1,51 @@
 <template>
-<h1 :class :style>Bonjour le monde !</h1>
+<input class="input"
+:class="{
+  inputOnGoing,
+  inputError,
+  inputValid
+}"
+
+@focus="input.focus =true; input.touched = true"
+@blur="input.focus = false"
+type="text"
+v-model="input.value">
+
 </template>
 
 <script setup lang="ts">
+import {computed, reactive, ref } from 'vue';
+
+const input = reactive({
+  value: '',
+  focus: false,
+  touched: false
+})
+
+const inputOnGoing = computed(() => input.focus && input.value.length)
+const inputError = computed(() => !input.focus && input.touched && input.value.length < 5)
+const inputValid = computed(() => input.touched && !input.focus && !inputError.value )
 
 </script>
 
 
 <style scoped>
-@import "./assets/base.css"
+
+.input {
+  outline: 0;
+  border: 10px solid black;
+  border-radius: 4px;
+}
+
+.inputOnGoing {
+  border-color: blue;
+}
+
+.inputError {
+  border-color: red;
+}
+
+.inputValid {
+  border-color: green;
+}
 </style>
