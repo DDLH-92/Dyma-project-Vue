9410a70d600c7b5e9e3619bc3f59d5cd6b8e9c84
David de la Harpe <118104692+DDLH-92@users.noreply.github.com>
2023-03-24 13:59:44 +0100

    test reactive1

--- dymavue/src/App.vue
+++ dymavue/src/App.vue
@@ -1,13 +1,30 @@
 <template>
-<input @keyup.enter="mykeyup">
 </template>
 
 <script setup lang="ts">
 
-function mykeyup() {
-  console.log('mykeyup');
+const obj = { name: 'Jean', age: 12 };
+
+function render() {
+  console.log('Should update template')
+}
+const proxy = new Proxy(obj, {
+  get(obj: any, prop) {
+    render();
+    return obj[prop];
+    }
+  });
+
+set(obj: any, prop, value) {
+  render ();
+  obj[prop] = value;
+  return true;
 }
 
+proxy.age = 15
+
+console.log(proxy.name);
+console.log(proxy.age);
 
 </script>
 
