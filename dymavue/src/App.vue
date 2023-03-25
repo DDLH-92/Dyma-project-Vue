fda1b6953e8f7a230cdf44a166348c0fc78c32da
David de la Harpe <118104692+DDLH-92@users.noreply.github.com>
2023-03-25 10:30:07 +0100

    composants

--- dymavue/src/App.vue
+++ dymavue/src/App.vue
@@ -1,11 +1,10 @@
 <template>
-  <h1 v-memo="[title]"><p>{{ title }}</p></h1>
-  <input type="text" v-model="input">
-  <p>{{ input }}</p>
+  <Product />
+  <Product />
+  <Product />
+  <Product />
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-const title = ref('Je suis un titre')
-const input = ref('')
+import Product from './Product.vue'
 </script>
